6f36d530bf0172a8699d4f9b1b162c220f953ae0
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-25 22:31:42 +0530

    Added: Creating Unique Coupon Code

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -7,18 +7,19 @@
             Console.WriteLine("This is Logical Programming Practice Problem");
 
 
-            Console.Write("Emter a number : ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            
-            int reverse = 0;
-            int temp = number;
-            while (number > 0)
+            Console.WriteLine("Enter the length of the copun code");
+            int length = Convert.ToInt32(Console.ReadLine());
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[length];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
             {
-                int rem = number % 10;
-                reverse = (reverse * 10) + rem;
-                number = number / 10;
+                stringChars[i] = chars[random.Next(chars.Length)];
             }
-            Console.WriteLine($"Reverse of the number {temp} is {reverse}");
+
+            var convString = new String(stringChars);
+            Console.WriteLine("The Unique Coupon Code is "+convString);
 
         }
     }
